f587dcfbc31552969bf044dbc594e73e0213968d
kimiaBadri <142110633+kimiaBadri@users.noreply.github.com>
2024-01-21 21:11:24 +0330

    finish read me and like

--- lib/screens/story_page.dart
+++ lib/screens/story_page.dart
@@ -1,4 +1,5 @@
 import 'package:blog_club/constants/constants.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class StoryPage extends StatelessWidget {
@@ -12,26 +13,74 @@ class StoryPage extends StatelessWidget {
         Container(
           decoration: BoxDecoration(color: blue),
         ),
-        Container(
-          width: double.infinity,
-          height: 690,
-          child: ClipRRect(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(20),
-              bottomRight: Radius.circular(20),
-            ),
-            child: FittedBox(
-              fit: BoxFit.fill,
-              child: Image.asset(
-                  'assets/images/stories/karl-magnuson-85J99sGggnw-unsplash.jpg'),
+        _getImageContainer(),
+        _getBottum(),
+        Positioned(
+          top: 770,
+          right: 36,
+          child: Column(
+            children: [
+              Icon(
+                CupertinoIcons.heart_fill,
+                color: Colors.red,
+                size: 40,
+              ),
+              Text(
+                '450K',
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _getImageContainer() {
+    return Container(
+      width: double.infinity,
+      height: 720,
+      child: ClipRRect(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(20),
+          bottomRight: Radius.circular(20),
+        ),
+        child: FittedBox(
+          fit: BoxFit.fill,
+          child: Image.asset(
+              'assets/images/stories/karl-magnuson-85J99sGggnw-unsplash.jpg'),
+        ),
+      ),
+    );
+  }
+
+  Widget _getBottum() {
+    return Positioned(
+      top: 770,
+      child: Row(
+        children: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                minimumSize: Size(130, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                backgroundColor: Colors.white),
+            onPressed: () {},
+            child: Center(
+              child: Text(
+                'Read More',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontFamily: 'AR',
+                  fontWeight: FontWeight.bold,
+                  color: blue,
+                ),
+              ),
             ),
           ),
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: Text('data'),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
