0646640b1182001e7a92afeda00e0a28310d78d9
kimiaBadri <142110633+kimiaBadri@users.noreply.github.com>
2024-01-22 21:13:27 +0330

    story-page

--- lib/screens/story_page.dart
+++ lib/screens/story_page.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:blog_club/constants/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -14,6 +15,49 @@ class StoryPage extends StatelessWidget {
           decoration: BoxDecoration(color: blue),
         ),
         _getImageContainer(),
+        Padding(
+          padding: const EdgeInsets.only(top: 100, left: 44),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _getStoryBox(),
+              SizedBox(
+                width: 16,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Jasmine Levin',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'AR',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    '4m ago',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontFamily: 'AR',
+                      fontWeight: FontWeight.w300,
+                      color: Colors.white60,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: 140,
+              ),
+              Icon(Icons.exit_to_app_sharp)
+            ],
+          ),
+        ),
+        _getBlurContainer(),
         _getBottum(),
         Positioned(
           top: 770,
@@ -80,6 +124,80 @@ class StoryPage extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _getBlurContainer() {
+    return Positioned(
+      top: 500,
+      child: ClipRRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: 14,
+            sigmaY: 14,
+          ),
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 32, right: 32),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Do You Want To Live A Happy Life? Smile.',
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontFamily: 'AR',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                      'Sometimes there’s no reason to smile, but I’ll smile anyway because of life. Yes, I’m one of those people who always smiles.',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'AR',
+                          fontWeight: FontWeight.w200,
+                          color: Colors.white60)),
+                ],
+              ),
+            ),
+            width: 310,
+            height: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color.fromRGBO(255, 255, 255, 0.1),
+                  Color.fromRGBO(255, 255, 255, 0.1)
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _getStoryBox() {
+    return Container(
+      width: 68,
+      height: 68,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(24),
+        ),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        ),
+        child: Image.asset('assets/images/stories/story_4.jpg'),
       ),
     );
   }
